0e9ac46562e4cb455b5e0cec035760a4a14ceba2
Jess Scheuring <jscheuring@gmail.com>
2014-09-15 19:16:57 -0400

    implemented ajax push request

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,50 @@
-console.log('The Iron Yard Rocks');
+var serverURL = '//tiny-pizza-server.herokuapp.com/collections/NicerHugs';
+
+var _id = "54174b6a84d2ee0200000057";
+
+
+// $.ajax({
+//     url: serverURL,
+//     type: 'GET'
+// })
+//     .done(function(a) {
+//         console.log(a);
+//     });
+//
+// $.ajax({
+//     url: serverURL + _id,
+//     type: 'delete'
+// }).done(function() {
+//     console.log('deleted')
+// });
+
+
+function sendNewTodo(e) {
+  e.preventDefault();
+  var todoObject =  {
+      title: $('#title').val(),
+      dueDate: Date.parse($('#due-date').val()),
+      createdDate: Date.now(),
+      description: $('#description').val(),
+      priority: $('#priority').val(),
+  };
+  $.ajax({
+      url: serverURL,
+      type: 'POST',
+      data: todoObject
+  })
+      .done(function() {
+          console.log('Sent!');
+      });
+}
+
+function renderTemplate(templateId, location, model) {
+    var templateString = $(templateId).text();
+    var templateFunction = _.template(templateString);
+    var renderedTemplate = templateFunction(model);
+    $(location).append(renderedTemplate);}
+
+renderTemplate('#form', '.form-element');
+
+
+$('#update-button').on('click', sendNewTodo);
